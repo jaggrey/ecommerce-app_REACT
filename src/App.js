9a5046020f385272ff2cd733ef88f5d94e182881
Jeffrey Aggrey <jeffreyaggrey@gmailcom>
2020-04-13 14:56:54 -0400

    Add files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Homepage from "./homepage.component";
+
+const App = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+};
+
+export default App;
